923aedc39945295cd2f0e3546651707617634549
Luke Trimble <lucasftrimble@gmail.com>
2020-02-20 14:43:36 -0800

    adds error message if no age input

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ $(document).ready(function()  {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
+  if (age) {  
     var quote = (100 - age) * 3;
     if (gender === 'male' || age < 26)  {
       quote += 50;
@@ -10,8 +11,11 @@ $(document).ready(function()  {
 
     $("#rate").empty().append(quote);
     $("#quote").show().show();
+  } else {
+    alert('Please enter your age.');
+  }
 
-    event.preventDefault();
+  event.preventDefault();
   });
 });
 
