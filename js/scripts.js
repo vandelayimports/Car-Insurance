a30014b264fc6059a4f0f54a1dae99fbf305ba46
Luke Trimble <lucasftrimble@gmail.com>
2020-02-20 14:27:11 -0800

    revises misandristic policy

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
-$(document).ready(fucntion()  {
+$(document).ready(function()  {
   $("form#insurance").submit(function(event)  {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
 
     var quote = (100 - age) * 3;
-    if (gender === 'male' && age < 26)  {
+    if (gender === 'male' || age < 26)  {
       quote += 50;
     }
 
     $("#rate").empty().append(quote);
-    $("#quote").show().show()
+    $("#quote").show().show();
 
     event.preventDefault();
   });
 });
+
